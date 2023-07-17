6cd37948e393a7c36050855866d3caaf791906d1
viniciusgit2 <4xv6patelfin@gmail.com>
2023-07-17 10:53:11 -0300

    o mega nem começou

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Card from "./componentes/Card"
 import Primeiro from"./componentes/Primeiro"
 import Segundo from './componentes/Segundo'
 import Frag from "./componentes/Frag"
-
+import Listaaluno from './componentes/Listaaluno';
 import Grupo from "./componentes/Grupo"
+import Parouimpar from './componentes/Parouimpar';
+import Input from './componentes/Input';
+import Contador from './componentes/contadador';
+import Soma from './componentes/Soma';
 function App() {
   return (
     <div className="App">
+    <Soma></Soma>
+      <Contador numero={0}></Contador>
+      <Input></Input>
           <Card></Card>
+          <Listaaluno></Listaaluno>
 <Primeiro></Primeiro>
 <Segundo></Segundo>
 <Frag></Frag>
 <Grupo></Grupo>
+<Parouimpar></Parouimpar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
